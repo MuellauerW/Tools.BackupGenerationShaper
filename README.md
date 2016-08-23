b5c1e615a8b4a6a8a02b093d3142b9afc90a502c
MuellauerW <w.muellauer@mdc.at>
2016-08-23 23:00:42 +0200

    Project converted to new GitHub Repo

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# Tools.BackupGenerationShaper
-Keep at least n geneations of timestamped backups
+# Tools.BackupGenerationShaper #
+
+## Revision History ##
+
+<table>
+  <tr> <th >Date</th>             <th>Author</th>  <th>Note</th>  </tr>
+  <tr> <td nowrap>2009-08-22</td>  <td>wm</td>  <td>first version of document created</td> </tr>
+  <tr> <td nowrap>2014-03-30</td> <td>	wm</td>	 <td>creation of git repository</td> </tr>
+  <tr> <td nowrap>2015-10-06</td> <td>wm</td>		<td>new name BackupGenerationShaper and neuw features as documented</td>  </tr>
+
+</table>
+
+## Introduction ##
+This tool helps keeping only the youngest n backup-files to avoid overflow of backup storage. For beging operated backup-files must contain a timestamp of defined format in their filename.
+The Applications supports the following timestamp formats:
+
+- yyyy_MM_dd_hh_mm_ss
+- yyyy_MM_dd_hh_mm
+- yyyy_MM_dd
+- yyyy-MM-dd-hh-mm-ss
+- yyyy-MM-dd-hh-mm
+- yyyy-MM-dd
+
+## How does the software do its job ##
+All directories that are enumerated in the .exe.config file (Section ShaperDirectires)
+are scanned and files are assigned to sets according to their filenames without timestamp.
+All distinct sets are sorted on timestamp and all older files that exced the number of files to be kept are deleted. 
+All activities are written to a log-file and in case of uncaught exceptions the software can send an email if configured to do so.
+
+## Configuration ##
+As this program is a .net application the configuration is done in an .exe.config file.
+To make things more elegant, I implemented a distinct configuration object hierarchy that is deserialized at startup. Configuration is done with an editor by manually editing
+the .exe.config file. Before altering this file save to old (working) config on a
+safe place so that you can revert to this version.
+Configuration looks as follows:
+
+## Layout of .exe.config Options ##
+
+- ShaperOptions
+  - ShaperDebugOptions
+    - IsSimulateOnly: bool
+	  - IsInteractive: bool
+	  - OnExceptionSendMail: bool
+	  - MailTo: string
+	  - MailFrom: string
+	  - MailSMTP: string
+  - ShaperLogOptions
+    - LogToScreen: bool
+    - LogToFile: bool
+	  - LoggingDirectory: string
+  - ShaperDirectories
+    - EntryList
+      - DirectoryName
+      - TimestampRegEx
+      - GenerationCount
+
---
+++ src/Spike_AppConfigSectionReader/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spike_AppConfigSectionReader
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Spike for learning handling .exe.config Filese");
+      Console.WriteLine($"This program will try to deserialize config data{Environment.NewLine}from a config section into a config object");
+      Console.WriteLine($"DefaultValue form .exe.config: \"{Properties.Settings.Default.DefaultValue}\" ");
+
+      //Hier wird die ShaperConfig aus dem userSettings herausgeholt. Aber so ist sie super gekapselt
+      Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+      ConfigurationSectionGroup userSettingsGroup = configuration.GetSectionGroup("userSettings");
+      ShaperConfig shaperConfig = userSettingsGroup.Sections["ShaperConfig"] as ShaperConfig;
+
+
+      if (shaperConfig != null) {
+        Console.WriteLine("Options Debug:");
+        Console.WriteLine($" Debug->IsVerbose: {shaperConfig.DebugOptions.IsVerbose}");
+        Console.WriteLine($" Debug->IsInteractive: {shaperConfig.DebugOptions.IsInteractive}");
+        Console.WriteLine($" Debug->OnSendMail: {shaperConfig.DebugOptions.OnExceptionSendMail}");
+        Console.WriteLine($" Debug->MailFrom: {shaperConfig.DebugOptions.MailFrom}");
+        Console.WriteLine($" Debug->MailTo: {shaperConfig.DebugOptions.MailTo}");
+        Console.WriteLine($" Debug->MailSMTP: {shaperConfig.DebugOptions.MailSMTP}");
+        Console.WriteLine("Options Log:");
+        Console.WriteLine($" Log->IsLogging: {shaperConfig.LogOptions.IsLogging}");
+        Console.WriteLine($" Log->LogDirectory: {shaperConfig.LogOptions.LogDirectory}");
+        Console.WriteLine("Options DirectoryList:");
+        foreach (ShaperDirectoryElement sde in shaperConfig.DirectoryList) {
+          Console.WriteLine($" Dir: {sde.DirectoryName} TS: {sde.TimestampFormat} Count: {sde.GenerationsCount}");
+        }
+        Console.WriteLine("Options End");
+      } else {
+        Console.WriteLine("NoConfig Found");
+      }
+
+      while (Console.KeyAvailable) { Console.ReadKey(true); }
+      Console.WriteLine("Press any key to terminate");
+      Console.ReadKey(true);
+
+    } //end     static void Main(string[] args)
+
+
+  } //end   class Program
+
+
+} //end namespace Spike_AppConfigSectionReader
+
+
